824f9c1d82fa5661164b4044244e7af7628c09e9
Natawipa Poonyakariyakorn <natawipa2005@gmail.com>
2025-09-01 20:53:23 +0700

    chore(backend): configure PostgreSQL connection

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,19 +11,23 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aw-*qz6iw3*3+bywv(o3x=kd^^b!ek^mabecj@0pda5t+i=@_%'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-x+_4yr_plxwk)@7^6w19pd46&na_9_gta(d1p7geeb0opr!quh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'ku_volunteer_db'),
+        'USER': os.getenv('DB_USER', 'ku_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'ku_password'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
