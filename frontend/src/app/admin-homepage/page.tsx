a9c9b5eec3560cba1e676e395a7191b7556202c6
Yatichapat <yatichapat.d@ku.th>
2025-10-02 16:29:11 +0700

    feature(frontend): add student and organization list page for admin

--- frontend/src/app/admin-homepage/page.tsx
+++ frontend/src/app/admin-homepage/page.tsx
@@ -10,10 +10,11 @@ import { useRef, useState, useEffect} from "react";
 
 // Fetch Data from example.json
 import eventsData from "../example.json";
-
+import students from "../studentExample.json";
+import organizations from "../organizerExample.json";
 const events = eventsData.events;
 
-// ------------------------------------
+// ------------------------------------ 
 const eventTypes = [
   {
     title: "กิจกรรมมหาวิทยาลัย",
@@ -176,12 +177,69 @@ export default function Home() {
         </Link>
 
         <h2 className="font-bold mb-6 text-2xl pt-6">User List</h2>
-        <Link href="admin/student-list">
-          <h3 className="font-semibold mb-4 text-xl bg-gradient-to-r from-blue-300/25 to-purple-300/25 p-5 w-full rounded-lg shadow-md hover:scale-102 transition-transform duration-200">Student List</h3>
+        <Link href={`admin/student-list`}>
+          <div className={`shadow-lg p-4 mb-10 rounded-lg relative overflow-hidden  bg-gradient-to-r from-green-300/25 to-purple-500/25 hover:scale-102 transition-transform duration-200`}>
+            {/* Background brain image */}
+            <Image
+                src={"/student.svg"}
+                alt="Student illustration"
+                width={200}
+                height={200}
+                className="absolute right-0 bottom-0 opacity-50 pointer-events-none"
+              />
+        
+            {/* Section title */}
+            <h2 className="font-medium mb-2 relative z-10 text-xl">
+              Student List <span className="text-gray-600">&gt;</span>
+            </h2>
+
+            {/* List of student names */}
+            <div className="space-y-3 px-4">
+               {students.slice().reverse().slice(0, 2).map((s, index) => (
+                <p
+                  key={s.id}
+                  className={`border-b border-gray-300 pb-2 ${
+                    index === 1 ? "text-gray-400" : "text-gray-700"
+                  }`}
+                >
+                  {s.title} {s.first_name} {s.last_name}
+                </p>
+              ))}
+            </div>
+          </div>
         </Link>
-        <Link href="admin/organization-list">
-          <h3 className="font-semibold mb-4 text-xl bg-gradient-to-r from-blue-300/25 to-purple-300/25 p-5 w-full rounded-lg shadow-md hover:scale-102 transition-transform duration-200">Organization List</h3>
-        </Link>
+        
+        <Link href="organization-list">
+          <div className={`shadow-lg p-4 mb-10 rounded-lg relative overflow-hidden  bg-gradient-to-r from-green-300/25 to-orange-400/25 hover:scale-102 transition-transform duration-200`}>
+            {/* Background brain image */}
+            <Image
+                src={"/organization.svg"}
+                alt="Organization illustration"
+                width={200}
+                height={200}
+                className="absolute right-0 bottom-0 opacity-60 pointer-events-none"
+              />
+        
+            {/* Section title */}
+            <h2 className="font-medium mb-2 relative z-10 text-xl">
+              Organization List <span className="text-gray-600">&gt;</span>
+            </h2>
+
+            {/* List of student names */}
+            <div className="space-y-3 px-4">
+               {organizations.slice().reverse().slice(0, 2).map((s, index) => (
+                <p
+                  key={s.id}
+                  className={`border-b border-gray-300 pb-2 ${
+                    index === 1 ? "text-gray-400" : "text-gray-700"
+                  }`}
+                >
+                  {s.organization_name}
+                </p>
+              ))}
+            </div>
+          </div>
+          </Link>
 
 
       </div>
