c9a23651116947acb1e2944ea308546906d5c5a9
Natawipa Poonyakariyakorn <natawipa2005@gmail.com>
2025-11-16 21:31:36 +0700

    feature(frontend): integrate Sentry for frontend monitoring

---
+++ frontend/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e9d9298721877832e8863711d91cdcf0@o4510369678622720.ingest.us.sentry.io/4510369691926529",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
